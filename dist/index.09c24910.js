875b62b2014e149bc275e2be422349f7af083bf3
tamimhasan19702 <tamimhasancu@gmail.com>
2022-08-20 15:12:01 +0600

    timeout function created

--- dist/index.09c24910.js
+++ dist/index.09c24910.js
@@ -1,2 +1,10 @@
+const recipeCounter = document.querySelector(".recipe");
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request Took to long to load! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
 
 //# sourceMappingURL=index.09c24910.js.map
---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const recipeCounter = document.querySelector('.recipe');
+
+const timeout = function(s){
+   return new Promise (function(_,reject){
+    setTimeout(function(){
+      reject(new Error(`Request Took to long to load! Timeout after ${s} second`));
+    }, s * 1000 );
+   });
+}
