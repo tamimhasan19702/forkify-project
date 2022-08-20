ea2104620ee8aa72cdff2cd39f50b1f8bef908eb
tamimhasan19702 <tamimhasancu@gmail.com>
2022-08-20 15:49:09 +0600

    called first api call for this project

--- dist/index.09c24910.js
+++ dist/index.09c24910.js
@@ -6,5 +6,28 @@ const timeout = function(s) {
         }, s * 1000);
     });
 };
+// ================================================
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        let { recipe  } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            imgredints: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 //# sourceMappingURL=index.09c24910.js.map
