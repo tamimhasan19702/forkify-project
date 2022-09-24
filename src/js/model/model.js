72cbba7ee9fd33dd7cd54a44c86f831c13760042
tamimhasan19702 <tamimhasancu@gmail.com>
2022-09-24 15:13:33 +0600

    erase bugeed code

--- src/js/model/model.js
+++ src/js/model/model.js
@@ -35,22 +35,4 @@ export const loadRecipe = async function (id){
 
 //recipe functionality
 
-export const loadSearchResults = async function(query){
-  try{
-  state.search.query = query;  
-  const data = await getJSON(`${API_URL}?search = ${query}`);
-  console.log(data);
-  state.search.results = data.data.recipes.map( rec => {
-    return {
-      id: rec.id,
-      title: rec.title,
-      publisher: rec.publisher,
-      image: rec.image_url
-    };
-  });
 
-  console.log(state.search.results);
-}catch(err){
-    throw err;
-  }
-}
--- src/js/views/resultsView.js
+++
@@ -1,7 +0,0 @@
-import View from "./View";
-
-class resultsView extends View{
-  _parentElement = document.querySelector('.results');
-}
-
-export default new resultsView()
